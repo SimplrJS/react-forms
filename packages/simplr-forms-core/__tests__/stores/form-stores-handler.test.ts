521a074a9487d628367cedd22e23633ac651f5e1
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-04-08 22:01:08 +0300

    Added form-stores-handler tests.

---
+++ packages/simplr-forms-core/__tests__/stores/form-stores-handler.test.ts
@@ -0,0 +1,54 @@
+import { FormStoresHandlerClass } from "../../src/stores/form-stores-handler";
+
+describe("Form stores handler", () => {
+    it("should get next store unique formId", () => {
+        let storesHandler = new FormStoresHandlerClass();
+        let a = storesHandler.NextStoreId();
+        let b = storesHandler.NextStoreId();
+
+        expect(a).toBeTruthy();
+        expect(typeof a).toBe("string");
+        expect(a).not.toBe(b);
+    });
+
+    it("should register with custom formId", () => {
+        let storesHandler = new FormStoresHandlerClass();
+        const FORM_ID = "custom-form-id";
+
+        expect(storesHandler.RegisterForm(FORM_ID)).toBe(FORM_ID);
+    });
+
+    it("should register and get generated formId", () => {
+        let storesHandler = new FormStoresHandlerClass();
+        let generatedFormId = storesHandler.RegisterForm();
+
+        expect(generatedFormId).toBeTruthy();
+        expect(typeof generatedFormId).toBe("string");
+    });
+
+    it("should return true if form store exists", () => {
+        let storesHandler = new FormStoresHandlerClass();
+        const FORM_ID = "custom-form-id";
+        const ANOTHER_FORM_ID = "another-custom-form-id";
+        storesHandler.RegisterForm(FORM_ID);
+
+        expect(storesHandler.Exists(FORM_ID)).toBe(true);
+        expect(storesHandler.Exists(ANOTHER_FORM_ID)).toBe(false);
+    });
+
+    it("should register and unregister form", () => {
+        let storesHandler = new FormStoresHandlerClass();
+        let generatedFormId = storesHandler.RegisterForm();
+
+        expect(storesHandler.Exists(generatedFormId)).toBe(true);
+        storesHandler.UnregisterForm(generatedFormId);
+        expect(storesHandler.Exists(generatedFormId)).toBe(false);
+    });
+
+    it("should get store", () => {
+        let storesHandler = new FormStoresHandlerClass();
+        let generatedFormId = storesHandler.RegisterForm();
+
+        expect(storesHandler.GetStore(generatedFormId)).toBeTruthy();
+    });
+});
