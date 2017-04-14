4ac2a112fca6ad8861d613e0a3b485574b125e82
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-04-14 15:46:10 +0300

    Started making Connector.

--- packages/simplr-validation/src/abstractions/base-validator.ts
+++ packages/simplr-validation/src/abstractions/base-validator.ts
@@ -2,13 +2,19 @@ import * as React from "react";
 
 import * as Contracts from "../contracts";
 
-export interface ValidatorProps { }
+export interface ValidatorProps {
+    errorMessage: string;
+}
 
 export abstract class BaseValidator<TProps extends ValidatorProps>
     extends React.Component<TProps, {}> implements Contracts.Validator {
     static SimplrValidationValidatorComponent(): void { }
 
     abstract Validate(value: any): Contracts.ValidationReturn;
+
+    protected SkipValidation(value: any) {
+        return (value == null || value === "");
+    }
 
     protected Valid(): Promise<void> {
         return new Promise<void>(resolve => {
