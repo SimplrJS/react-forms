c1cc333d8d346c698810e8fce96173cb2a2e869e
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-04-18 12:14:36 +0300

    Sandboxed sinon method spying.

--- packages/simplr-validation/__tests__/subscribers/form-store-subscriber.test.ts
+++ packages/simplr-validation/__tests__/subscribers/form-store-subscriber.test.ts
@@ -1,12 +1,32 @@
-import { Stores } from "simplr-forms-core";
-import { spy } from "sinon";
+import { Stores, Actions } from "simplr-forms-core";
+import * as Sinon from "sinon";
+import * as ActionEmitter from "action-emitter";
 
 import { FormStoreSubscriber } from "../../src/subscribers/form-store-subscriber";
 
+let sandbox: Sinon.SinonSandbox;
 describe("FormStoreSubscriber", () => {
+    beforeEach(() => {
+        sandbox = Sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
     it("add listeners to form store", () => {
         const formStore = new Stores.FormStore("form-id");
-        const callback = spy(Stores.FormStore.prototype, "addListener");
+        const callback = sandbox.stub(Stores.FormStore.prototype, "addListener");
+
+        expect(callback.called).toEqual(false);
+        new FormStoreSubscriber(formStore);
+        console.warn(formStore.listeners(Actions.PropsChanged).length);
+        expect(callback.called).toEqual(true);
+    });
+
+    it("add listeners to form store", () => {
+        const formStore = new Stores.FormStore("form-id");
+        const callback = sandbox.stub(Stores.FormStore.prototype, "addListener");
 
         expect(callback.called).toEqual(false);
         new FormStoreSubscriber(formStore);
