93f4a5dcda6e394e1c77651edce9eba0f4653194
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-04-19 15:11:58 +0300

    Added some validators.

---
+++ packages/simplr-validation/src/validators/alphanumeric.ts
@@ -0,0 +1,23 @@
+import * as Validator from "validator";
+import { Contracts } from "simplr-forms-core";
+
+import { BaseValidator, ValidatorProps } from "../abstractions/base-validator";
+import { ValidationResult } from "../contracts";
+
+export interface AlphanumericValidatorProps extends ValidatorProps {
+    locale?: ValidatorJS.AlphanumericLocale;
+}
+
+export class AlphanumericValidator extends BaseValidator<AlphanumericValidatorProps> {
+    Validate(value: Contracts.FieldValue): ValidationResult {
+        if (this.SkipValidation(value)) {
+            return this.ValidSync();
+        }
+
+        if (!Validator.isAlphanumeric(value, this.props.locale)) {
+            return this.InvalidSync(this.props.errorMessage);
+        }
+
+        return this.ValidSync();
+    }
+}
---
+++ packages/simplr-validation/src/validators/boolean.ts
@@ -0,0 +1,21 @@
+import * as Validator from "validator";
+import { Contracts } from "simplr-forms-core";
+
+import { BaseValidator, ValidatorProps } from "../abstractions/base-validator";
+import { ValidationResult } from "../contracts";
+
+export interface BooleanValidatorProps extends ValidatorProps { }
+
+export class BooleanValidator extends BaseValidator<BooleanValidatorProps> {
+    Validate(value: Contracts.FieldValue): ValidationResult {
+        if (this.SkipValidation(value)) {
+            return this.ValidSync();
+        }
+
+        if (!Validator.isBoolean(value)) {
+            return this.InvalidSync(this.props.errorMessage);
+        }
+
+        return this.ValidSync();
+    }
+}
