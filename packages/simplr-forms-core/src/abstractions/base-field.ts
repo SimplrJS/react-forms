008ff9c1a01888530fffd249091e7c9919a11302
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-20 23:40:03 +0300

    prop-types package added to simpmlr-forms-core.

--- packages/simplr-forms-core/src/abstractions/base-field.ts
+++ packages/simplr-forms-core/src/abstractions/base-field.ts
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as actionEmitter from "action-emitter";
+import * as PropTypes from "prop-types";
 
 import {
     FieldProps,
@@ -37,9 +38,9 @@ export abstract class BaseField<TProps extends FieldProps, TState extends BaseFi
     public context: ParentContext;
 
     static contextTypes: React.ValidationMap<ParentContext> = {
-        FormId: React.PropTypes.string,
-        FormProps: React.PropTypes.object,
-        FieldsGroupId: React.PropTypes.string,
+        FormId: PropTypes.string,
+        FormProps: PropTypes.object,
+        FieldsGroupId: PropTypes.string,
         // FieldsGroupProps: React.PropTypes.object
     };
 
