b1890a2712215e68598e1ffa920762573f01983b
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-21 16:47:40 +0300

    Publish package started. Git repository dirtyness checking implemented.

---
+++ packages/simplr-forms-publish/src/index.ts
@@ -0,0 +1,36 @@
+import * as git from "nodegit";
+import * as process from "process";
+
+process.chdir("../simplr-forms-dom");
+
+function statusToText(status: any) {
+    var words = [];
+    if (status.isNew()) { words.push("[new]"); }
+    if (status.isModified()) { words.push("[modified]"); }
+    if (status.isTypechange()) { words.push("[typechange]"); }
+    if (status.isRenamed()) { words.push("[renamed]"); }
+    if (status.isIgnored()) { words.push("[ignored]"); }
+
+    return words.join(" ");
+}
+
+async function ensureRepositoryIsClean(repository: any) {
+    const statuses = await repository.getStatus();
+    if (statuses != null && statuses.length > 0) {
+        console.error(`Your repository is dirty:`);
+        for (const file of statuses) {
+            console.info(`\t${file.path()} ${statusToText(file)}`);
+        }
+        console.info("");
+        console.error("Repository must be clean before publishing.");
+        console.info("Exiting...");
+        process.exit(1);
+    }
+}
+
+async function run() {
+    const repository = await git.Repository.open("../../");
+    ensureRepositoryIsClean(repository);
+}
+
+run();
