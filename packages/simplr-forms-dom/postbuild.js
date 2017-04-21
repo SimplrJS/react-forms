a405c41f4119d5ef4171f45bb28454c42feb834f
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-21 18:12:02 +0300

    postbuild script updated.

--- packages/simplr-forms-dom/postbuild.js
+++ packages/simplr-forms-dom/postbuild.js
@@ -1,8 +1,35 @@
 const mv = require("mv");
+const fs = require("mz/fs");
+const path = require("path");
 const process = require("process");
-mv("dist", ".", {}, (err) => {
-    if (err) {
-        process.exit(1);
+
+async function move(from, to, recursively = true, rootFrom) {
+    from = path.resolve(from);
+    to = path.resolve(to);
+
+    rootFrom = rootFrom || from;
+
+    const files = await fs.readdir(from);
+    for (const file of files) {
+        const stats = await fs.stat(path.join(from, file));
+        if (await stats.isFile()) {
+            const fileResolved = path.resolve(from, file);
+            const fromDirectory = path.dirname(fileResolved);
+            const toDirectory = fromDirectory.replace(rootFrom, to);
+            const toFile = path.join(toDirectory, fileResolved.replace(fromDirectory, ""));
+            await fs.rename(fileResolved, toFile);
+        }
+        if (await stats.isDirectory()) {
+            if (recursively === true) {
+                await move(path.join(from, file), to, recursively, from);
+            }
+        }
     }
-    process.exit(0);
-});
+}
+
+async function run() {
+    const distPath = "./dist";
+    await move(distPath, ".", true);
+}
+
+run();
