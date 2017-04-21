05fbcf4dee672234a492181e1fdfedc581f0a657
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-22 01:46:07 +0300

    postbuild script finished.

--- packages/simplr-forms-dom/postbuild.js
+++ packages/simplr-forms-dom/postbuild.js
@@ -2,8 +2,9 @@ const mv = require("mv");
 const fs = require("mz/fs");
 const path = require("path");
 const process = require("process");
+const mkdirp = require("mkdirp");
 
-async function move(from, to, recursively = true, rootFrom) {
+async function move(from, to, recursively = true, removeFromDirectory = true, rootFrom = undefined) {
     from = path.resolve(from);
     to = path.resolve(to);
 
@@ -17,19 +18,44 @@ async function move(from, to, recursively = true, rootFrom) {
             const fromDirectory = path.dirname(fileResolved);
             const toDirectory = fromDirectory.replace(rootFrom, to);
             const toFile = path.join(toDirectory, fileResolved.replace(fromDirectory, ""));
-            await fs.rename(fileResolved, toFile);
+
+            try {
+                await mkdirpAsync(toDirectory);
+                await fs.rename(fileResolved, toFile);
+            } catch (err) {
+                console.log(err);
+            }
         }
         if (await stats.isDirectory()) {
             if (recursively === true) {
-                await move(path.join(from, file), to, recursively, from);
+                const fromDirectory = path.join(from, file);
+                await move(fromDirectory, to, recursively, removeFromDirectory, from);
+                if (removeFromDirectory === true && await fs.exists(fromDirectory)) {
+                    await fs.rmdir(fromDirectory);
+                }
             }
         }
     }
+    if (removeFromDirectory === true && await fs.exists(from)) {
+        await fs.rmdir(from);
+    }
+}
+
+async function mkdirpAsync(dir) {
+    return new Promise((resolve, reject) => {
+        mkdirp(dir, (err, made) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(made);
+        });
+    });
 }
 
 async function run() {
     const distPath = "./dist";
-    await move(distPath, ".", true);
+    await move(distPath, "./dist2", true);
 }
 
 run();
