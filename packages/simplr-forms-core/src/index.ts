b01d16de97b4a2f80cb9319114e0e66fc8ec6a6e
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-24 04:48:22 +0300

    Main file named exports updated to export * from abstractions @ simplr-forms-core.

--- packages/simplr-forms-core/src/index.ts
+++ packages/simplr-forms-core/src/index.ts
@@ -1,13 +1,1 @@
-import * as Abstractions from "./abstractions/index";
-import * as Actions from "./actions/index";
-import * as Contracts from "./contracts/index";
-import * as Stores from "./stores/index";
-import * as Utils from "./utils/index";
-
-export {
-    Abstractions,
-    Actions,
-    Contracts,
-    Stores,
-    Utils
-};
+export * from "./abstractions/index";
