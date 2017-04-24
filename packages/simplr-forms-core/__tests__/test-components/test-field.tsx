f40ed4a54eac3d6c21c8f446ad1af8fad46d27e1
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-24 04:16:25 +0300

    InitialValue -> DefaultValue @ tests. DefaultValue added while registering field @ tests.

--- packages/simplr-forms-core/__tests__/test-components/test-field.tsx
+++ packages/simplr-forms-core/__tests__/test-components/test-field.tsx
@@ -5,6 +5,7 @@ import { FieldProps, FieldValue } from "../../src/contracts/field";
 
 export interface MyFieldProps extends FieldProps {
     value?: string;
+    defaultValue?: string;
     randomKey?: string;
 }
 
