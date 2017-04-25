70f25830b5989cae1262a05e0ae055300442b9cc
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-25 03:34:33 +0300

    Feature/dom (#19).

--- packages/simplr-forms-dom/src/components/form.tsx
+++ packages/simplr-forms-dom/src/components/form.tsx
@@ -1,17 +1,46 @@
 import * as React from "react";
 import { BaseForm } from "simplr-forms-core";
-import { FormProps } from "simplr-forms-core/contracts";
-import { OnChangeCallback } from "../contracts/field";
-import { FormOnSubmitCallback } from "../contracts/form";
 
-export interface FormProps extends FormProps, React.HTMLProps<HTMLFormElement> {
-    onSubmit?: FormOnSubmitCallback;
-    onChange?: OnChangeCallback<any>;
-}
+import { FormProps, FormOnSubmitCallback } from "../contracts/form";
 
 export class Form extends BaseForm<FormProps, {}> {
+    public Element: HTMLFormElement;
+
+    static defaultProps: FormProps = {
+        preventSubmitDefaultAndPropagation: true
+    };
+
+    protected SetElementRef = (element: HTMLFormElement) => {
+        this.Element = element;
+        this.FormStore.SetSubmitCallback(() => {
+            element.dispatchEvent(new Event("submit"));
+        });
+    }
+
+    protected FormSubmitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
+        if (this.props.preventSubmitDefaultAndPropagation) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        if (!this.ShouldFormSubmit()) {
+            return;
+        }
+
+        // TODO: Touch all fields to validate
+
+        if (this.props.onSubmit == null) {
+            return;
+        }
+
+        const result = this.props.onSubmit(event, this.FormStore);
+        this.Submit(result);
+    }
+
     render() {
-        return <form>
+        return <form
+            ref={this.SetElementRef}
+            onSubmit={this.FormSubmitHandler}
+        >
             {this.props.children}
         </form>;
     }
