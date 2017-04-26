ae890ee67eccfcb744bf27bed674a86867391370
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-04-26 16:19:48 +0300

    Added form component tests.

--- packages/simplr-forms-dom/__tests__/components/form.test.tsx
+++ packages/simplr-forms-dom/__tests__/components/form.test.tsx
@@ -2,22 +2,37 @@ import * as React from "react";
 import { mount } from "enzyme";
 import * as sinon from "sinon";
 
-import { FormStore } from "simplr-forms-core/stores";
+import { FormStore, FSHContainer, FormStoresHandler } from "simplr-forms-core/stores";
 
 import { Form } from "../../src/components/form";
 import { Text } from "../../src/components/text";
 import { FormOnSubmitCallback } from "../../src/contracts/form";
 
 describe("Form", () => {
-    it("calls submit callback", () => {
+    beforeEach(() => {
+        FSHContainer.SetFormStoresHandler(new FormStoresHandler(), true);
+    });
+
+    it("calls submit callback when submit button is clicked", () => {
         const submitCallback = sinon.stub();
 
         const wrapper = mount(<Form onSubmit={submitCallback}>
-            <Text name="username" />
             <button type="submit">Submit</button>
         </Form>);
 
         wrapper.find("button").simulate("submit");
+
+        expect(submitCallback.called).toBe(true);
+    });
+
+    it("calls submit callback when submit called from FormStore", () => {
+        const formId = "form-id";
+        const submitCallback = sinon.stub();
+
+        const wrapper = mount(<Form formId={formId} onSubmit={submitCallback}></Form>);
+
+        const formStore = FSHContainer.FormStoresHandler.GetStore(formId);
+        formStore.InitiateSubmit();
 
         expect(submitCallback.called).toBe(true);
     });
