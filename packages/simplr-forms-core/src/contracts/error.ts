ae1df53667e0d3a8c8d0210c9a9bb28866a32fd5
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-05-04 17:02:13 +0300

    Added Origin to FormErorr.

--- packages/simplr-forms-core/src/contracts/error.ts
+++ packages/simplr-forms-core/src/contracts/error.ts
@@ -1,4 +1,11 @@
 // Future self might love usage of an object with message property instead of a plain string
 export interface FormError {
     Message: string;
+    Origin?: FormErrorOrigin; // Told you so.
+}
+
+export enum FormErrorOrigin {
+    None = 0,
+    Validation = 1 << 0,
+    Submit = 1 << 1
 }
