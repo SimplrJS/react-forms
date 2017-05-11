70975ec1294c4bfd5e2177b29c19adac15580f39
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-05-11 18:38:05 +0300

    Fixed validation.

--- packages/simplr-validation/src/utils/validation.ts
+++ packages/simplr-validation/src/utils/validation.ts
@@ -1,27 +1,53 @@
 import { FieldValue } from "simplr-forms/contracts";
-import { ProcessValue } from "simplr-forms/utils";
+import {
+    ProcessValue,
+    IsComponentOfType,
+    RenderComponents,
+    ConstructFormError
+} from "simplr-forms/utils";
 
-import { Validator, FIELD_VALIDATOR_FUNCTION_NAME, FORM_VALIDATOR_FUNCTION_NAME } from "../contracts";
+import {
+    Validator,
+    FIELD_VALIDATOR_FUNCTION_NAME,
+    FORM_VALIDATOR_FUNCTION_NAME
+} from "../contracts";
 
 
-export function ValidateField(components: Array<JSX.Element>, value: FieldValue) {
-    return ProcessValue<Validator, Promise<void>>(components, value, FIELD_VALIDATOR_FUNCTION_NAME,
-        async (processor, value) => {
-            const validationResult = processor.Validate(value);
-            if (validationResult != null && typeof validationResult === "string") {
-                throw validationResult;
-            }
-            return await validationResult;
-        });
+function IsPromise<T>(value: any): value is Promise<T> {
+    return value != null && value.then != null && value.catch != null;
 }
 
-export function ValidateForm(components: Array<JSX.Element>, value: FieldValue) {
-    return ProcessValue<Validator, Promise<void>>(components, value, FORM_VALIDATOR_FUNCTION_NAME,
-        async (processor, value) => {
-            const validationResult = processor.Validate(value);
-            if (validationResult != null && typeof validationResult === "string") {
-                throw validationResult;
-            }
-            return await validationResult;
-        });
+export async function ValidateValue(
+    components: Array<JSX.Element>,
+    value: any,
+    validatorTypeFunctionName: string) {
+    const validators = components.filter(x => IsComponentOfType(x, validatorTypeFunctionName));
+    const renderedValidators = RenderComponents<Validator>(validators);
+
+    for (const validator of renderedValidators) {
+        const validationResult = validator.Validate(value);
+        // Ensure that we have a promise
+        let promise: Promise<void>;
+        if (IsPromise<void>(validationResult)) {
+            promise = validationResult;
+        } else {
+            promise = new Promise<void>((resolve, reject) => {
+                const error = ConstructFormError(validationResult);
+                if (error !== undefined) {
+                    reject(validationResult);
+                    return;
+                }
+                resolve();
+            });
+        }
+        await promise;
+    }
+}
+
+export function ValidateField(components: Array<JSX.Element>, value: FieldValue) {
+    return ValidateValue(components, value, FIELD_VALIDATOR_FUNCTION_NAME);
+}
+
+export function ValidateForm(components: Array<JSX.Element>, value: any) {
+    return ValidateValue(components, value, FORM_VALIDATOR_FUNCTION_NAME);
 }
