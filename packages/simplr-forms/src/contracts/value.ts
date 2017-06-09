88a737f0818bdfe8662dbc10ca0708804272e698
Dovydas Navickas <dovydas@quatrodev.com>
2017-06-09 03:30:19 +0300

    TransitionalValue added to form-store and modifiers.

--- packages/simplr-forms/src/contracts/value.ts
+++ packages/simplr-forms/src/contracts/value.ts
@@ -1,8 +1,13 @@
 import { FieldValue } from "./field";
 
+export interface ModifierValue {
+    Value: FieldValue;
+    TransitionalValue?: FieldValue;
+}
+
 export interface Modifier {
     Format(value: FieldValue): FieldValue;
-    Parse(value: FieldValue): FieldValue;
+    Parse(value: ModifierValue): ModifierValue;
 }
 
 export interface Normalizer {
---
+++ packages/simplr-forms/src/modifiers/string-to-decimal.ts
@@ -0,0 +1,103 @@
+import { FieldValue } from "../contracts/field";
+import { ValueOfType } from "../utils/value-helpers";
+import { BaseModifier } from "./base-modifier";
+import { ModifierValue } from "../contracts/value";
+
+export interface StringToDecimalProps {
+    delimiter?: string;
+    precision?: number;
+}
+
+const EMPTY_VALUE = "";
+
+export class StringToDecimalModifier extends BaseModifier<StringToDecimalProps, {}> {
+    static defaultProps: StringToDecimalProps = {
+        delimiter: "."
+    };
+
+    private static emptyFormattedValue: string;
+
+    Format(value: FieldValue): FieldValue {
+        console.log(value);
+        if (value !== EMPTY_VALUE) {
+            return value.toString();
+        }
+        return "";
+    }
+    Parse(modifierValue: ModifierValue): ModifierValue {
+        let value = modifierValue.TransitionalValue != null ? modifierValue.TransitionalValue : modifierValue.Value;
+
+        if (value.length === 0) {
+            return {
+                Value: 0,
+                TransitionalValue: ""
+            };
+        }
+        if (ValueOfType<string>(modifierValue, StringToDecimalModifier.name, "object")) {
+            const firstChar = value.substr(0, 1);
+            let negative = false;
+            if (firstChar === "-") {
+                negative = true;
+                value = value.substr(1);
+
+                // If there is only a minus sign
+                if (value.length === 0) {
+                    return {
+                        Value: 0,
+                        TransitionalValue: "-"
+                    };
+                }
+            }
+
+            const leadingMinus = negative ? "-" : "";
+            const regex = new RegExp(`[^0-9\\${this.props.delimiter}]+`, "g");
+            console.log(`Delimiter: ${this.props.delimiter}`);
+            const extractedValue: string = this.LeaveOnlyFirstDelimiter(
+                value.replace(regex, ""),
+                this.props.delimiter!);
+
+            let transitionalValue = this.TrimLeft(extractedValue, "0");
+
+            // Add leading zero, if fraction is being entered
+            if (transitionalValue.substr(0, 1) === this.props.delimiter) {
+                transitionalValue = "0" + transitionalValue;
+            }
+
+            if (this.props.precision != null) {
+                const delimiterIndex = transitionalValue.indexOf(this.props.delimiter!);
+                console.log(`Delimiter index: ${delimiterIndex}`);
+                if (delimiterIndex !== -1) {
+                    transitionalValue = transitionalValue.substr(0, delimiterIndex + this.props.precision + 1);
+                }
+            }
+
+            transitionalValue = leadingMinus + transitionalValue;
+            let numValue = Number(transitionalValue);
+
+            return {
+                Value: numValue,
+                TransitionalValue: transitionalValue != numValue.toString() ? transitionalValue : undefined
+            };
+        }
+        return {
+            Value: 0
+        };
+    }
+
+    protected TrimLeft(value: string, symbol: string): string {
+        const arr = Array.from(value);
+        while (arr.length > 0 && arr[0] === symbol) {
+            arr.splice(0, 1);
+        }
+        return arr.join("");
+    }
+
+    protected LeaveOnlyFirstDelimiter(value: string, delimiter: string) {
+        if (value.indexOf(delimiter) === -1) {
+            return value;
+        }
+
+        const [start, ...rest] = value.split(delimiter);
+        return start + delimiter + rest.join("");
+    }
+}
