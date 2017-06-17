ee2b2efb84c974a9456179a42dacac705a5c8ac1
Dovydas Navickas <dovydas@quatrodev.com>
2017-06-18 01:05:56 +0300

    Validation initialization shortcut function.

--- packages/simplr-validation/src/index.ts
+++ packages/simplr-validation/src/index.ts
@@ -1,1 +1,9 @@
 export * from "./validators/index";
+export * from "./subscribers/index";
+
+import { ValidationSubscriberContainer } from "./subscribers/index";
+
+export function InitializeValidation() {
+    // A shortcut function
+    ValidationSubscriberContainer.Initialize();
+}
