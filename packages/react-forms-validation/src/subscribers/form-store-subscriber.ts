93decbd5846d56333b5a4b1b45edf85da9460cb2
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-06-20 17:18:59 +0300

    Validation: Added OnBlur (#97)
    * Added new Validation type OnBlur. Added missing actions FieldValidated, FieldActive.
    
    * Added new action FieldBlurred.
    
    * Added OnBlur validation.
    
    * Updated npm shrinkwrap.
    
    * Added FieldBlurred action test.

--- packages/react-forms-validation/src/subscribers/form-store-subscriber.ts
+++ packages/react-forms-validation/src/subscribers/form-store-subscriber.ts
@@ -9,7 +9,9 @@ import { FormStore } from "@simplr/react-forms/stores";
 import {
     FieldRegistered,
     ValueChanged,
-    FieldPropsChanged
+    FieldPropsChanged,
+    FieldActive,
+    FieldBlurred
 } from "@simplr/react-forms/actions";
 
 import { ValidateField, ValidateForm } from "../utils/validation";
@@ -19,23 +21,27 @@ export class FormStoreSubscriber {
     private fieldOnRegisteredSubscription?: ActionEmitter.EventSubscription;
     private fieldOnValueChangedSubscription?: ActionEmitter.EventSubscription;
     private fieldOnPropsChangedSubscription?: ActionEmitter.EventSubscription;
+    private fieldsOnBlurSubscription?: ActionEmitter.EventSubscription;
 
     constructor(private formStore: FormStore) {
         this.fieldOnRegisteredSubscription = this.formStore.addListener(FieldRegistered, this.OnRegistered.bind(this));
         this.fieldOnValueChangedSubscription = this.formStore.addListener(ValueChanged, this.OnValueChanged.bind(this));
         this.fieldOnPropsChangedSubscription = this.formStore.addListener(FieldPropsChanged, this.OnPropsChanged.bind(this));
+        this.fieldsOnBlurSubscription = this.formStore.addListener(FieldBlurred, this.OnBlur.bind(this));
     }
 
     public RemoveFormListeners(): void {
         if (this.fieldOnRegisteredSubscription != null) {
             this.fieldOnRegisteredSubscription.remove();
         }
-
         if (this.fieldOnValueChangedSubscription != null) {
             this.fieldOnValueChangedSubscription.remove();
         }
         if (this.fieldOnPropsChangedSubscription != null) {
             this.fieldOnPropsChangedSubscription.remove();
+        }
+        if (this.fieldsOnBlurSubscription != null) {
+            this.fieldsOnBlurSubscription.remove();
         }
     }
 
@@ -99,5 +105,10 @@ export class FormStoreSubscriber {
     protected async OnPropsChanged(action: FieldPropsChanged): Promise<void> {
         await this.ValidateField(action.FieldId, FieldValidationType.OnValueChange);
         await this.ValidateForm(FieldValidationType.OnValueChange);
+    }
+
+    protected async OnBlur(action: FieldBlurred): Promise<void> {
+        await this.ValidateField(action.FieldId, FieldValidationType.OnBlur);
+        await this.ValidateForm(FieldValidationType.OnBlur);
     }
 }
--- packages/react-forms/src/contracts/validation.ts
+++ packages/react-forms/src/contracts/validation.ts
@@ -2,5 +2,6 @@ export enum FieldValidationType {
     None,
     OnFieldRegistered = 1 << 1,
     OnValueChange = 1 << 2,
-    OnPropsChange = 1 << 3
+    OnPropsChange = 1 << 3,
+    OnBlur = 1 << 4
 }
