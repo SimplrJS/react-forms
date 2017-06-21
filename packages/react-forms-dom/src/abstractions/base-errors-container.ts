ca3316b7232bd92e61554831c0fdda5f8b4c3f67
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-06-21 17:29:59 +0300

    Added BaseErrorsContainer and ErrorsContainer.

---
+++ packages/react-forms-dom/src/abstractions/base-errors-container.ts
@@ -0,0 +1,56 @@
+import { BaseContainer, BaseContainerParentContext } from "@simplr/react-forms";
+import { FormStore } from "@simplr/react-forms/stores";
+import { FormError, FieldValidationStatus } from "@simplr/react-forms/contracts";
+
+export type BaseErrorsContainerProps = {};
+
+export type FieldErrors = { [id: string]: FormError };
+
+export interface BaseErrorsContainerState {
+    FieldErrors: FieldErrors;
+    FormError?: FormError;
+}
+
+export abstract class BaseErrorsContainer<TProps extends BaseErrorsContainerProps, TState extends BaseErrorsContainerState>
+    extends BaseContainer<TProps, TState> {
+    public state: TState = {
+        FieldErrors: {}
+    } as TState;
+
+    protected OnStoreUpdated(): void {
+        const storeState = this.FormStore.GetState();
+
+        if (!storeState.HasError) {
+            this.setState(state => {
+                state.FieldErrors = {};
+                state.FormError = undefined;
+                return state;
+            });
+            return;
+        }
+        const formError = storeState.Form.Error;
+        const fieldsWithError = storeState
+            .FieldsValidationStatuses
+            .filter(x => x != null && x === FieldValidationStatus.HasError)
+            .keySeq()
+            .toArray();
+
+        const fieldsErrors: FieldErrors = {};
+
+        fieldsWithError.forEach(fieldId => {
+            const fieldError = storeState.Fields.get(fieldId).Error;
+            if (fieldError != null) {
+                fieldsErrors[fieldId] = fieldError;
+            }
+        });
+
+        this.setState(state => {
+            state.FormError = formError;
+            state.FieldErrors = fieldsErrors;
+
+            return state;
+        });
+    }
+
+    public abstract render(): JSX.Element | null;
+}
