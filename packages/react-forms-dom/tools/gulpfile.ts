b9e779a6c8411a61d7c81b79f58717e4d88e6748
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-07-08 23:26:55 +0300

    Added extractor packages: react-forms-dom and react-forms-validation.

--- packages/react-forms-dom/tools/gulpfile.ts
+++ packages/react-forms-dom/tools/gulpfile.ts
@@ -5,7 +5,9 @@ import * as webpackConfig from "./webpack.config";
 import * as process from "process";
 import * as childProcess from "child_process";
 import * as simplrMvDir from "@simplr/mvdir";
+import { ExtractorTaskClass } from "@simplr/extractor";
 
+// tslint:disable-next-line:no-require-imports
 const packageJson = require("./package.json");
 
 const compiler = webpack(webpackConfig);
@@ -18,7 +20,7 @@ const paths = {
     root: "."
 };
 
-export function webpackTask() {
+export function webpackTask(): Promise<{}> {
     return new Promise((resolve, reject) => {
         compiler.run((err, stats) => {
             if (err != null) {
@@ -30,29 +32,40 @@ export function webpackTask() {
     });
 }
 
-export async function moveFromDistTask() {
+export async function moveFromDistTask(): Promise<void> {
     const from = path.resolve(paths.dist);
     const to = path.resolve(paths.root);
     await simplrMvDir.move(from, to);
 }
 
-export async function copyToJspm() {
-    const jspmPath = `../react-forms-test/dist/jspm_packages/npm/@simplr/react-forms-dom@${packageJson.version}`;
+export async function copyToJspm(): Promise<void> {
+    const jspmPath = `../react-forms-test/dist/jspm_packages/npm/${packageJson.name}@${packageJson.version}`;
     const outputPath = path.resolve(jspmPath);
     gulp.src(packageJson.files)
         .pipe(gulp.dest(outputPath));
 }
 
-// TODO: Fix any when proper gulp types will be available.
-const buildTask = gulp.series(webpackTask, moveFromDistTask) as any;
+const buildTask = gulp.series(webpackTask, moveFromDistTask);
 
-export function watchStartTask() {
-    // TODO: Fix any when proper gulp types will be available.
-    gulp.watch(paths.src, gulp.series(buildTask, copyToJspm) as any);
+export function watchStartTask(): void {
+    gulp.watch(paths.src, gulp.series(buildTask, copyToJspm));
 }
 
 export const build = buildTask;
 export const watch = gulp.series(buildTask, copyToJspm, watchStartTask);
+
+export async function createJSON(): Promise<void> {
+    const config: webpack.Configuration = webpackConfig;
+    const modules = config.entry as webpack.Entry;
+
+    for (const moduleName in modules) {
+        if (modules.hasOwnProperty(moduleName)) {
+            const extractor = new ExtractorTaskClass(__dirname);
+            const modulePath = modules[moduleName];
+            extractor.JSONGenerator(`./api/${moduleName}.api.json`, modulePath.toString());
+        }
+    }
+}
 
 // tslint:disable-next-line:no-default-export
 export default buildTask;
