d5ce4c2655f0e16461356b29f84f6ac040d04b60
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-02-20 16:40:13 +0200

    Hydration, Unregister tests.

--- packages/react-forms/src/stores/__tests__/form-store.test.ts
+++ packages/react-forms/src/stores/__tests__/form-store.test.ts
@@ -1,4 +1,10 @@
-import { FormStore } from "../form-store";
+import { FormStore, FormStoreState, FormStoreData } from "../form-store";
+
+class TestFormStore extends FormStore {
+    public getTestState(): FormStoreState {
+        return this.getState();
+    }
+}
 
 const FAKE_FORM_ID = "fake-form-id";
 const FAKE_FIELD_ID = "fake-field-id";
@@ -8,6 +14,28 @@ it("Register a field", () => {
     store.registerField(FAKE_FIELD_ID);
 
     expect(store.getFields()).toMatchSnapshot();
+});
+
+it("Unregister a field", () => {
+    const store = new FormStore(FAKE_FORM_ID);
+    store.registerField(FAKE_FIELD_ID);
+    expect(store.getFields()).toMatchSnapshot();
+
+    store.unregisterField(FAKE_FIELD_ID);
+    expect(store.getFields()).toMatchSnapshot(); 
+});
+
+it("Hydration", () => {
+    const store = new TestFormStore(FAKE_FORM_ID);
+    store.hydrate({
+        fields: {
+            "field-1": {
+                value: "field value 1"
+            }
+        }
+    });
+
+    expect(store.getTestState()).toMatchSnapshot();
 });
 
 it("Dehydration", () => {
