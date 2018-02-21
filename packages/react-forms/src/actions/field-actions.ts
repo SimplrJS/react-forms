a1435958365933e345675b626c1d6af82e5bd5e8
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-02-21 19:59:38 +0200

    Updated Actions.

--- packages/react-forms/src/actions/field-actions.ts
+++ packages/react-forms/src/actions/field-actions.ts
@@ -1,10 +1,10 @@
 import { StoreHydrated } from "./store-actions";
 
 export abstract class FieldBaseAction {
-    constructor(private fieldId: string) {}
+    constructor(private $fieldId: string) {}
 
-    public get FieldId(): string {
-        return this.fieldId;
+    public get fieldId(): string {
+        return this.$fieldId;
     }
 }
 
@@ -13,21 +13,21 @@ export class FieldRegistered extends FieldBaseAction {}
 export class FieldUnregistered extends FieldBaseAction {}
 
 export class FieldValueChanged extends FieldBaseAction {
-    constructor(fieldId: string, private value: string) {
+    constructor(fieldId: string, private $value: string) {
         super(fieldId);
     }
 
-    public get Value(): string {
-        return this.value;
+    public get value(): string {
+        return this.$value;
     }
 }
 
 export class FieldStoreHydrated extends StoreHydrated {
-    constructor(private fieldId: string) {
+    constructor(private $fieldId: string) {
         super();
     }
 
-    public get FieldId(): string {
-        return this.fieldId;
+    public get fieldId(): string {
+        return this.$fieldId;
     }
 }
--- packages/react-forms/src/actions/form-actions.ts
+++ packages/react-forms/src/actions/form-actions.ts
@@ -1,27 +1,27 @@
 import { StoreHydrated } from "./store-actions";
 
 export class FormRegistered {
-    constructor(private formId: string) {}
+    constructor(private $formId: string) {}
 
-    public get FormId(): string {
-        return this.formId;
+    public get formId(): string {
+        return this.$formId;
     }
 }
 
 export class FormUnregistered {
-    constructor(private formId: string) {}
+    constructor(private $formId: string) {}
 
-    public get FormId(): string {
-        return this.formId;
+    public get formId(): string {
+        return this.$formId;
     }
 }
 
 export class FormStoreHydrated extends StoreHydrated {
-    constructor(private formId: string) {
+    constructor(private $formId: string) {
         super();
     }
 
-    public get FormId(): string {
-        return this.formId;
+    public get formId(): string {
+        return this.$formId;
     }
 }
