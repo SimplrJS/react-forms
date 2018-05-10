6bf1ef7e44783844fe8bcc19e0dc5019794baca5
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-05-10 12:55:03 +0300

    Added rollup.config.js to git.

---
+++ packages/react-forms-dom/rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from "rollup-plugin-typescript2";
+import uglify from "rollup-plugin-uglify";
+import autoExternal from "rollup-plugin-auto-external";
+
+// `npm run build` -> `ROLLUP_WATCH` is false
+// `npm run watch` -> `ROLLUP_WATCH` is true
+const production = !process.env.ROLLUP_WATCH;
+
+let plugins;
+if (production) {
+    plugins = [
+        uglify()
+    ];
+} else {
+    plugins = [];
+}
+
+export default {
+    input: "./src/index.ts",
+    output: {
+        file: "index.js",
+        format: "cjs",
+        name: "react-forms-dom"
+    },
+    plugins: [
+        typescript(),
+        autoExternal(),
+        ...plugins
+    ]
+}
