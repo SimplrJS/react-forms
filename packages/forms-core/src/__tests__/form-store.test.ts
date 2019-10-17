6117fa66c8905be88dc5bd1e76303e16ec53f952
Dovydas Navickas <dovydas@quatrodev.com>
2019-10-17 13:54:33 +0300

    Initial state updating tests.

--- packages/forms-core/src/__tests__/form-store.test.ts
+++ packages/forms-core/src/__tests__/form-store.test.ts
@@ -7,10 +7,45 @@ describe("form-store", () => {
         store = new FormStore();
     });
 
-    it("update return-style", () => {
+    it("update by draft mutation", () => {
+        // Arrange
+        const oldState = store.getState();
+        
+        // TODO: Sanity check? Can this be done according to AAA testing?
+        expect(oldState.status.disabled).toBe(false);
+
         const updater: FormStateUpdater<FormState> = state => {
             state.status.disabled = true;
         };
+
+        // Act
         store.update(updater);
+
+        // Assert
+        const updatedState = store.getState();
+        expect(updatedState).not.toBe(oldState);
+        expect(updatedState.status.disabled).toBe(true);
+    });
+
+    it("update by returning new state", () => {
+        // Arrange
+        const oldState = store.getState();
+        const updater: FormStateUpdater<FormState> = state => {
+            return {
+                ...state,
+                status: {
+                    ...state.status,
+                    disabled: true
+                }
+            };
+        };
+
+        // Act
+        store.update(updater);
+
+        // Assert
+        const updatedState = store.getState();
+        expect(updatedState).not.toBe(oldState);
+        expect(updatedState.status.disabled).toBe(true);
     });
 });
