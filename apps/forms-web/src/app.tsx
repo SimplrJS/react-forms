2d1a175d82a8e8f88db7d22cf82ec19806e9539c
Dovydas Navickas <dovydas@quatrodev.com>
2019-10-30 21:41:06 +0200

    Initial forms-web configured.

---
+++ apps/forms-web/src/app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = (): JSX.Element => {
+    return <div>Hello forms!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
